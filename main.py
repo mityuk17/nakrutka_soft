1c9a32206710bc30272c4fbafe3d84f6f92f9f15
mityuk17 <mityukov.17@gmail.com>
2023-01-18 22:18:12 +0300

    123

--- main.py
+++ main.py
@@ -13,7 +13,26 @@ def get_sessions():
 def get_proxies():
     with open('proxies.txt', 'r') as file:
         data = file.readlines()
+    for i in range(len(data)):
+        data[i] = data[i].strip().replace('::', ':').split(':')
+        data[i] = {'http': f'http://{data[i][2]}:{data[i][3]}@{data[i][0]}:{data[i][1]}/'}
     return data
-def get_target():
+def get_targets():
     with open('targets.txt', 'r') as file:
-        file
+        data = file.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].strip()
+    return data
+sessions = get_sessions()
+print(sessions)
+targets = get_targets()
+proxies = get_proxies()
+def raspred_proxies():
+    accs = list()
+    counter = 0
+    for i in range(len(sessions)):
+        accs.append([proxies[counter],sessions[i]])
+        counter += 1
+        if counter == len(proxies) - 1:
+            counter = 0
+    return accs
